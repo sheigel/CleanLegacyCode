14f1f38dcc8f84984a61920fa2641eb5b6a0c0de
Silviu Eigel <eigel.si@gmail.com>
2014-04-26 16:55:07 +0300

    PublisherUnknown 1st failing test

--- LegacyCode.Tests/BookOverviewTests.cs
+++ LegacyCode.Tests/BookOverviewTests.cs
@@ -1,10 +1,22 @@
-﻿using NUnit.Framework;
+﻿using LegacyCode.Bll;
+using NUnit.Framework;
 
 namespace LegacyCode.Tests
 {
     [TestFixture]
     public class BookOverviewTests
     {
-         
+        [TestFixture]
+        public class PublisherUnknown : BookOverviewTests
+        {
+            [Test]
+            public void DisplayBooksGroupedByPublisher()
+            {
+                var sut = new BookOverview();
+
+                sut.DisplayFilteredBooks();
+
+            }
+        }
     }
 }
